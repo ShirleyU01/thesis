45e31a3d19a62b54f314014e9a31b3a8a8477f37
Ruben Martins <rubenm@andrew.cmu.edu>
2025-02-07 11:22:19 -0500

    Update to test LLMs

--- llms/main.py
+++ llms/main.py
@@ -7,12 +7,16 @@ from llm import LLM
 def main():
     # Check if the user provided the correct number of arguments
     if len(sys.argv) != 2:
-        print("Usage: python main.py <integer>")
+        print("Usage: python main.py <Level:integer>")
+        print("Level: 0 - Prompt options: Basic")
+        print("Level: 1 - Prompt options: Basic + Syntax")
+        print("Level: 2 - Prompt options: Basic + Library")
+        print("Level: 3 - Prompt options: Basic + Diversity")
+        print("Level: 4 - Prompt options: Basic + Syntax + Library + Diversity")
         sys.exit(1)
 
-
     # Open the CSV file and read its contents
-    with open('../prompt/description.csv', mode='r') as file:
+    with open('../prompt/description_mini.csv', mode='r') as file:
         reader = csv.reader(file)
         
         # Skip the header row if your CSV file has one
@@ -24,8 +28,8 @@ def main():
                 column1, column2, column3, column4, column5 = row
                 #print(row)
 
-                #llm = LLM(model="gpt4o-mini", litellm_url="http://0.0.0.0:4000", master_key="anything",temperature=0.7)
-                llm = LLM(model="gpt4o", litellm_url="http://0.0.0.0:4000", master_key="anything",temperature=0.7)
+                llm = LLM(model="gpt4o-mini", litellm_url="http://0.0.0.0:4000", master_key="anything",temperature=0.7)
+                #llm = LLM(model="gpt4o", litellm_url="http://0.0.0.0:4000", master_key="anything",temperature=0.7)
 
                 # Create an instance of IdeasGeneration
                 ideas_generator = IdeasGeneration(level=int(sys.argv[1]),nb_ideas=10,attempts=3,llm=llm,description=column2,filename=column1,function_name=column3,module_name=column4,signature=column5)
